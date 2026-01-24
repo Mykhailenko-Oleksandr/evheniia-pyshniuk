514f394b0ce21970fd0dcbb6a0f15c65e69e33bb
Sapper <saniamykhailenko@gmail.com>
2026-01-24 11:08:56 +0200

    add: all item in prices page and mobile styles

--- components/PricesPlans/PricesPlans.tsx
+++ components/PricesPlans/PricesPlans.tsx
@@ -15,16 +15,22 @@ export default function PricesPlans() {
               height={200}
             />
             <div className={css.contentBox}>
-              <h2 className={css.contentTitle}>Базовий (Технічний проект)</h2>
+              <h2 className={css.contentTitle}>
+                Базовий <span>(Технічний проект)</span>
+              </h2>
               <ul className={css.contentList}>
                 <li className={css.contentItem}>
-                  <p className={css.text}>Обмір</p>
+                  <p className={css.text}>- Обмір</p>
                 </li>
                 <li className={css.contentItem}>
-                  <p className={css.text}>Розробка 3-х варіантів планування</p>
+                  <p className={css.text}>
+                    - Розробка 3-х варіантів планування
+                  </p>
                 </li>
                 <li className={css.contentItem}>
-                  <p className={css.text}>Пакет професійних креслень:</p>
+                  <p className={`${css.text} ${css.packageTitle}`}>
+                    - Пакет професійних креслень:
+                  </p>
                   <ul className={css.contentListSecond}>
                     <li className={css.listSecondItem}>
                       <p className={css.text}>Обмірний план</p>
@@ -59,8 +65,107 @@ export default function PricesPlans() {
               </div>
             </div>
           </li>
-          {/* <li className={css.item}>2</li>
-          <li className={css.item}>3</li> */}
+          <li className={css.item}>
+            <Image
+              className={css.image}
+              src="/images/prices/heroPriceTab.webp"
+              alt="Сучасна вітальня з сірою софою, круглими столиками та обідньою зоною в мінімалістичному стилі"
+              width={300}
+              height={200}
+            />
+            <div className={css.contentBox}>
+              <h2 className={css.contentTitle}>Оптимальний пакет</h2>
+              <ul className={css.contentList}>
+                <li className={css.contentItem}>
+                  <p className={css.text}>- Обмір</p>
+                </li>
+                <li className={css.contentItem}>
+                  <p className={css.text}>
+                    - Розробка 3-х варіантів планування
+                  </p>
+                </li>
+                <li className={css.contentItem}>
+                  <p className={`${css.text} ${css.packageTitle}`}>
+                    - Пакет професійних креслень
+                  </p>
+                </li>
+                <li className={css.contentItem}>
+                  <p className={css.text}>
+                    - Специфікація оздоблювальних матеріалів та предметів
+                    інтер'єру
+                  </p>
+                </li>
+                <li className={css.contentItem}>
+                  <p className={css.text}>
+                    - Колажі з дизайн-концепцією для кожного приміщення
+                  </p>
+                </li>
+              </ul>
+              <div className={css.bottomBox}>
+                <div className={css.bottomTextBox}>
+                  <p className={css.bottomBoxTextTop}>Термін виконання:</p>
+                  <p className={css.bottomBoxTextBottom}>2-2.5 міс</p>
+                </div>
+                <div className={css.bottomTextBox}>
+                  <p className={css.bottomBoxTextTop}>Вартість:</p>
+                  <p className={css.bottomBoxTextBottom}>10$ /м2</p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li className={css.item}>
+            <Image
+              className={css.image}
+              src="/images/prices/heroPriceTab.webp"
+              alt="Сучасна вітальня з сірою софою, круглими столиками та обідньою зоною в мінімалістичному стилі"
+              width={300}
+              height={200}
+            />
+            <div className={css.contentBox}>
+              <h2 className={css.contentTitle}>Повний комплекс</h2>
+              <ul className={css.contentList}>
+                <li className={css.contentItem}>
+                  <p className={css.text}>- Обмір</p>
+                </li>
+                <li className={css.contentItem}>
+                  <p className={css.text}>
+                    - Розробка 3-х варіантів планування
+                  </p>
+                </li>
+                <li className={css.contentItem}>
+                  <p className={`${css.text} ${css.packageTitle}`}>
+                    - Пакет професійних креслень
+                  </p>
+                </li>
+                <li className={css.contentItem}>
+                  <p className={css.text}>
+                    - Специфікація оздоблювальних матеріалів та предметів
+                    інтер'єру
+                  </p>
+                </li>
+                <li className={css.contentItem}>
+                  <p className={css.text}>
+                    - 3D-візуалізації кожного приміщення
+                  </p>
+                </li>
+                <li className={css.contentItem}>
+                  <p className={css.text}>
+                    - Альбом креслень (у друкованому та електронному форматах)
+                  </p>
+                </li>
+              </ul>
+              <div className={css.bottomBox}>
+                <div className={css.bottomTextBox}>
+                  <p className={css.bottomBoxTextTop}>Термін виконання:</p>
+                  <p className={css.bottomBoxTextBottom}>3.5-4 міс</p>
+                </div>
+                <div className={css.bottomTextBox}>
+                  <p className={css.bottomBoxTextTop}>Вартість:</p>
+                  <p className={css.bottomBoxTextBottom}>17$ /м2</p>
+                </div>
+              </div>
+            </div>
+          </li>
         </ul>
       </div>
     </section>
